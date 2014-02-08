73b57a58d96545c1b11a02a9f4c2b19df34d9951
Innocent Ekeh <Innoekeh@gmail.com>
2014-02-07 16:17:23 -0800

    some changes in sublime to do with s3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,9 @@ class Pin < ActiveRecord::Base
 
      has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100>" }
 
+:storage => :s3,
+            :bucket => 'AwesomeInno',
+            :s3_credentials => {:access_key_id => ENV['AKIAI3XBHV4L5BKAM2CA'], :secret_access_key => ENV['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']}
 
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
   validates :image, presence: true
